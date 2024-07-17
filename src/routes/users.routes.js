2cff7695671bb2802915d9ccad4b3d78be8e2f10
kennedy <sferreira.kennedy@gmail.com>
2024-07-17 13:25:17 +0100

    Update users.routes.js

--- src/routes/users.routes.js
+++ src/routes/users.routes.js
@@ -5,5 +5,6 @@ const usersRouter = Router()
 const userController = new UserController();
 
 usersRouter.post('/', userController.create)
+usersRouter.put('/:id', userController.update)
 
 module.exports = usersRouter
